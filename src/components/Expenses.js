bbadb1495e0f2e610869c1a8c31cac637ba52411
Celal Karakoc <c.karakoc@live.nl>
2023-07-07 13:32:25 +0200

    Added Card wrapper component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css"
 
 export default function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {props.expenses.map((expense) =>
                 <ExpenseItem
                     title={expense.title}
                     amount={expense.amount}
                     date={expense.date}
                 />)}
-        </div>
+        </Card>
     );
 }
