a9ae1dfadb6d3c5afe8af1558cd7ec5ab8348551
Celal Karakoc <c.karakoc@live.nl>
2023-07-14 17:03:30 +0200

    Added filtering based on year - Assignment 2.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 export default function Expenses(props) {
+    const [filteredYear, setFilteredYear] = useState(2021);
+
+    function filterChangeHandler(year) {
+        setFilteredYear(year);
+    }
+
     return (
-        <Card className="expenses">
-            {props.expenses.map((expense) =>
-                <ExpenseItem
-                    title={expense.title}
-                    amount={expense.amount}
-                    date={expense.date}
-                />)}
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear} />
+
+                {props.expenses.filter(expense => expense.date.getFullYear() === filteredYear).map(expense =>
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                )}
+            </Card>
+        </div>
     );
 }
