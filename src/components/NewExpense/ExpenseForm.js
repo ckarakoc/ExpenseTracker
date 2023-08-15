e61bcecd73ef2b475f4ae6f89ae25ef0f0fe554b
Celal Karakoc <c.karakoc@live.nl>
2023-08-15 09:33:47 +0200

    Assignment 4

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -102,6 +102,7 @@ export default function ExpenseForm(props) {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,28 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
+import { useState } from 'react';
 
 export default function NewExpense(props) {
+    const [isEditing, setIsEditing] = useState(false);
+
     function onSaveExpenseDataHandler(enteredExpenseData) {
         const expenseData = {
-            ...enteredExpenseData, 
+            ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+            {isEditing ? (<ExpenseForm
+                onSaveExpenseData={onSaveExpenseDataHandler}
+                onCancel={() => setIsEditing(false)}
+            />) : (
+                <button onClick={() => setIsEditing(true)}>Add New Expense</button>
+            )}
+
         </div>
     );
 };
